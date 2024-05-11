1bc91c156d12c42002c67269beb2781cb97dcc5d
vshlz <vshappo2004@gmail.com>
2024-05-11 16:31:50 +0300

    separate content and remove random logic

---
+++ content.js
@@ -0,0 +1,9 @@
+content = [
+    // Viikko 1
+    {
+     "text": "Test event",
+     "image": "https://hips.hearstapps.com/hmg-prod/images/when-does-summer-end-1651614198.jpg?crop=1xw:0.8612204724409449xh;center,top",
+     "map":  {"lat": 60.1699, "lng": 24.9384},
+     "video": "I8ACAPIzG1g?si=TN0s9QZOxi7dl0TE"
+    }
+];
--- script.js
+++ script.js
@@ -11,36 +11,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const modalMap = document.getElementById('modal-map');
     const close = document.getElementsByClassName("close")[0];
 
-    content = [
-        {
-         "text": "Test test test",
-         "image": "https://hips.hearstapps.com/hmg-prod/images/when-does-summer-end-1651614198.jpg?crop=1xw:0.8612204724409449xh;center,top",
-         "map":  {"lat": 60.1699, "lng": 24.9384},
-         "video": "I8ACAPIzG1g?si=TN0s9QZOxi7dl0TE"
-        }
-    ];
+    // content = [
+    //     {
+    //      "text": "Test test test",
+    //      "image": "https://hips.hearstapps.com/hmg-prod/images/when-does-summer-end-1651614198.jpg?crop=1xw:0.8612204724409449xh;center,top",
+    //      "map":  {"lat": 60.1699, "lng": 24.9384},
+    //      "video": "I8ACAPIzG1g?si=TN0s9QZOxi7dl0TE"
+    //     }
+    // ];
 
-    weeks.forEach(week => {
+    weeks.forEach((week, index) => {
         week.addEventListener('click', function() {
-            const randomContent = content[Math.floor(Math.random() * content.length)];
-            modalText.textContent = randomContent.text || '';
-            modalText.style.display = randomContent.text ? 'block' : 'none';
+            const weekContent = content[index];
+            modalText.textContent = weekContent.text || '';
+            modalText.style.display = weekContent.text ? 'block' : 'none';
 
-            modalImage.src = randomContent.image || '';
-            modalImage.style.display = randomContent.image ? 'block' : 'none';
+            modalImage.src = weekContent.image || '';
+            modalImage.style.display = weekContent.image ? 'block' : 'none';
 
             
-            console.log(randomContent.video)
-            if (randomContent.video) {
-                // modalVideo.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/S1yOYOkOLMA?si=-NGYh3DBHAFcY-DF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
-                modalVideo.innerHTML = `<iframe height="100%" width="100%" src="https://www.youtube.com/embed/${randomContent.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+            console.log(weekContent.video)
+            if (weekContent.video) {
+                modalVideo.innerHTML = `<iframe height="100%" width="100%" src="https://www.youtube.com/embed/${weekContent.video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                 modalVideo.style.display = 'block';
             } else {
                 modalVideo.style.display = 'none';
             }
 
-            if (randomContent.map) {
-                updateOrCreateMap(modalMap, randomContent.map);
+            if (weekContent.map) {
+                updateOrCreateMap(modalMap, weekContent.map);
                 modalMap.style.display = 'block';
                 setTimeout(function() {
                     myMap.invalidateSize(); // Adjust the map size
