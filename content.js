ceeb722d83e6ff0149d6583d4103e6f5534424b1
vshlz <vshappo2004@gmail.com>
2024-05-15 23:07:18 +0300

    Update Events

--- content.js
+++ content.js
@@ -1,9 +1,93 @@
 content = [
     // Viikko 1
     {
-     "text": "Test event",
-     "image": "https://hips.hearstapps.com/hmg-prod/images/when-does-summer-end-1651614198.jpg?crop=1xw:0.8612204724409449xh;center,top",
-     "map":  {"lat": 60.1699, "lng": 24.9384},
-     "video": "I8ACAPIzG1g?si=TN0s9QZOxi7dl0TE"
-    }
+        "text": "SEA LIFE",
+        "image": "https://www.visitsealife.com/helsinki/media/gfibd23o/sea-life-helsinki3.jpg?anchor=center&mode=crop&format=webp&quality=80&width=500&height=250",
+        "map": { "lat": 60.2088702, "lng": 24.8966399 },
+        "video": "z-Xu-72IK3k?si=Y3iBfYHIWZltluMZ"
+    },
+    // Viikko 2
+    {
+        "text": "LINNANMÄKI",
+        "image": "https://huvipuistoblogi.fi/wp-content/uploads/2022/06/linnanmaki_logo-1024x768.jpg",
+        "map": { "lat": 60.2098557, "lng": 24.8966399 },
+        "video": "OAGfde6SPMU?si=eR_hF6BkUk8x8cc8"
+    },
+    // Viikko 3
+    {
+        "text": "PRIDE",
+        "image": "https://pride.fi/wp-content/uploads/2021/05/Alejandro-Lorenzo_kulkue15-scaled-1.jpg",
+        "map": { "lat": 60.1685719, "lng": 24.8648262 },
+        "video": "G4RZGwnyviU?si=UoHJNgVKGBTokhVb"
+    },
+    // Viikko 4
+    {
+        "text": "KIASMA",
+        "image": "https://kiasma.fi/wp-content/uploads/sites/2/2020/11/kiasma_generic.jpg",
+        "map": { "lat": 60.2010285, "lng": 24.8962488 },
+        "video": "cifFyTwidQE?si=M2ChzzK8cQN7eO2F"
+    },
+    // Viikko 5
+    {
+        "text": "ATENEUM",
+        "image": "https://ateneum.fi/wp-content/uploads/sites/3/2022/02/Ateneum_2000px_14_1_22_cVille_Malja-23.jpg",
+        "map": { "lat": 60.2314214, "lng": 25.0249847 },
+        "video": "J-lZNAzAcuI?si=crpN4oyU3MrLFoCP"
+    },
+    // Viikko 6
+    {
+        "text": "PUSKA FESTIVAL",
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpqh70VaZl2lqvgiKSbMrlHudbiZnQSgmP1ZSRrA4VA&s",
+        "map": { "lat": 60.219837, "lng": 24.8698367 },
+        "video": "6xZ8srusVew?si=jU4wf97KWLYo_-bn"
+    },
+    // Viikko 7
+    {
+        "text": "SUOMENLINNA",
+        "image": "https://frantic.s3.eu-west-1.amazonaws.com/suomenlinna/2014/06/kuvitus_ilmakuvarajattu-1600x706.jpg",
+        "map": { "lat": 60.1448318, "lng": 24.9043356 },
+        "video": "xzlcbWB6XOg?si=rNWrv7PDCe3lz4en"
+    },
+    // Viikko 8
+    {
+        "text": "WEEKEND FESTIVAL",
+        "image": "https://www.wknd.fi/content/uploads/2023/08/WKND23_Day2_223432_HeikkiSalonen_-e1691418938904.jpg",
+        "map": { "lat": 60.215884, "lng": 24.7581838 },
+        "video": "tVpuh4BNhDA?si=O1kEyyaNrvV9vNbo"
+    },
+    // Viikko 9
+    {
+        "text": "FLOW FESTIVAL",
+        "image": "https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2016/08/10/1331811349704_2/flow",
+        "map": { "lat": 60.1851083, "lng": 24.9634539 },
+        "video": "81GfQyoycBE?si=qvIeT9dzu2SIRnEt"
+    },
+    // Viikko 10
+    {
+        "text": "LUONNONTIETEELLINEN MUSEO",
+        "image": "https://museo.fi/wp-content/uploads/2023/04/luomus_luonnontieteellinen_museo_dinosaurusten_maailma_kuva_emma_suominen_1.jpg",
+        "map": { "lat": 60.2015952, "lng": 24.9036949 },
+        "video": "jX2xDw4EubY?si=lRqUdNQ3p8d-9fAw"
+    },
+    // Viikko 11
+    {
+        "text": "KORKEASAARI",
+        "image": "https://xb-static.nm-ovp.nelonenmedia.fi/xb/styles/640x360/public/img/c6f482b270a49bccfaa06b446f18b4654f95efe2-10476.jpg",
+        "map": { "lat": 60.1748879, "lng": 24.9359523 },
+        "video": "__X2QPZdp8w?si=oJjua66eQPOsp8sr"
+    },
+    // Viikko 12
+    {
+        "text": "TEMPPELIAUKION KIRKKO",
+        "image": "https://projektiuutiset.fi/wp-content/uploads/2015/11/Q1W4267.jpg",
+        "map": { "lat": 60.2007033, "lng": 24.891211 },
+        "video": "Vua-WTTfcCU?si=pr0LSGSR3BXD6kO8"
+    },
+    // Viikko 13
+    {
+        "text": "OODI",
+        "image": "https://ala.fi/wordpress/wp-content/uploads/2019/01/TU-181231-ala-oodi-071-screen-1140x759.jpg",
+        "map": { "lat": 60.2011139, "lng": 24.8962483 },
+        "video": "f3MPmnpl1lI?si=zWNRS3UIa8V3-SYF"
+    },
 ];
